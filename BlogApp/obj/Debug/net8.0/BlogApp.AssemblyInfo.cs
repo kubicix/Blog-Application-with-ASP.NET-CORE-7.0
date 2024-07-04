5e4f887022f97d1716eca7c75b35252877256a7e
Kubilay Birer <kubilaybirer@hotmail.com>
2024-07-04 13:13:08 +0300

    Created seed data class for automatically adding data at the start instead of adding manually

--- BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net8.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a02d6919ddfed446a99587121cf759620fa6b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667e45945abd2f9fa327de306e0750bc2e9f139b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

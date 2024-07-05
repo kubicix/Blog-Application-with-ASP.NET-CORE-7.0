ee1c3b71409c7b428a5c131815a790ee5c0f0f7d
Kubilay Birer <kubilaybirer@hotmail.com>
2024-07-05 13:03:13 +0300

    added blog posts controller and listed posts on index page successfully

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -3,15 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<BlogContext>(options =>{
-    var config = builder.Configuration;
-    var connectionString=config.GetConnectionString("sql_connection");
-    options.UseSqlite(connectionString);
+    
+    options.UseSqlite(builder.Configuration["ConnectionStrings:Sql_connection"]);
 });
 
 var app = builder.Build();
 SeedData.TestVerileriniDoldur(app);
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
 
 app.Run();
